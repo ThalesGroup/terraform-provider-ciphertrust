451026059990957b35026dbfa52ee5d23db404a2
Thales Group <terraform-issues-trac-aaaaesy24gyllanpdgdnezbohq@thalescpl.slack.com>
2023-06-28 00:05:04 +0000

    updated docs

--- sample-scripts/google-ekm-endpoints/main.tf
+++ sample-scripts/google-ekm-endpoints/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ciphertrust = {
       source = "thales.com/terraform/ciphertrust"
-      version = "0.9.0-beta9"
+      version = "0.9.0-beta10"
     }
   }
 }
