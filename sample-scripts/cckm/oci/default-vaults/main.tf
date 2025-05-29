c63c412b35c08795b76cd4dd71ecb7ffe10707ef
Sarah <sarah.thompson@thalesgroup.com>
2025-05-29 15:25:13 +1000

    Update sample script for oci vault

--- sample-scripts/cckm/oci/default-vaults/main.tf
+++ sample-scripts/cckm/oci/default-vaults/main.tf
@@ -7,31 +7,32 @@ terraform {
   }
 }
 
-provider "ciphertrust" {}
+provider "ciphertrust" {
 
-resource "random_id" "random" {
-  byte_length = 8
 }
 
-locals {
-  connection_name          = "tf-oci-vaults-${lower(random_id.random.hex)}"
-  path_to_private_key_file = "./oci-pk.pem"
-  public_key_fingerprint   = "ed:e9:80:b0:58:b8:73:fd:b4:72:b7:58:2e:59:cc:34"
+resource "ciphertrust_oci_connection" "connection" {
+  name                = "example-connection-name"
+  key_file            = "path-to-or-contents-of-the-private-key-file-"
+  pub_key_fingerprint = "public-key-fingerprint"
+  region              = "oci-region"
+  tenancy_ocid        = "tenancy-ocid"
+  user_ocid           = "user-ocid"
 }
 
 # Use the ciphertrust_get_oci_regions datasource to get a list of regions
 data "ciphertrust_get_oci_regions" "regions" {
-  connection_id = ciphertrust_oci_connection.oci_connection.name
+  connection_id = ciphertrust_oci_connection.connection.name
 }
 
 # Use the ciphertrust_get_oci_compartments datasource to get a list of compartments
 data "ciphertrust_get_oci_compartments" "compartments" {
-  connection_id = ciphertrust_oci_connection.oci_connection.name
+  connection_id = ciphertrust_oci_connection.connection.name
 }
 
 # Use the ciphertrust_get_oci_vaults datasource to get a list of available vaults
 data "ciphertrust_get_oci_vaults" "vaults" {
-  connection_id  = ciphertrust_oci_connection.oci_connection.name
+  connection_id  = ciphertrust_oci_connection.connection.name
   compartment_id = data.ciphertrust_get_oci_compartments.compartments.compartments.0.id
   region         = data.ciphertrust_get_oci_regions.regions.regions.0
 }
@@ -39,7 +40,7 @@ data "ciphertrust_get_oci_vaults" "vaults" {
 # Create an OCI vault using information obtained from above datasources
 resource "ciphertrust_oci_vault" "vault" {
   region        = data.ciphertrust_get_oci_regions.regions.regions.0
-  connection_id = ciphertrust_oci_connection.oci_connection.name
+  connection_id = ciphertrust_oci_connection.connection.name
   vault_id      = data.ciphertrust_get_oci_vaults.vaults.vaults.0.vault_id
 }
 
@@ -49,6 +50,9 @@ data "ciphertrust_oci_vault_list" "vault_by_name" {
     name = ciphertrust_oci_vault.vault.name
   }
 }
+output "vault_by_name" {
+  value = data.ciphertrust_oci_vault_list.vault_by_name
+}
 
 # List a CipherTrust Manager OCI vault by resource ID
 data "ciphertrust_oci_vault_list" "vault_by_id" {
@@ -56,8 +60,14 @@ data "ciphertrust_oci_vault_list" "vault_by_id" {
     id = ciphertrust_oci_vault.vault.id
   }
 }
+output "vault_by_id" {
+  value = data.ciphertrust_oci_vault_list.vault_by_id
+}
 
 # List all CipherTrust Manager OCI vaults
 data "ciphertrust_oci_vault_list" "vault_no_filters" {
   depends_on = [ciphertrust_oci_vault.vault]
+}
+output "vault_no_filters" {
+  value = data.ciphertrust_oci_vault_list.vault_no_filters
 }
