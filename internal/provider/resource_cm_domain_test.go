8835dec7c7135d6f25b60574724d9205bb13d1f6
Rishabh Sharma <rishabh-i.sharma@cplcloud.com>
2026-03-25 15:45:07 +0530

    fix TestResourceCMDomain test

--- internal/provider/resource_cm_domain_test.go
+++ internal/provider/resource_cm_domain_test.go
@@ -1,34 +1,40 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestResourceCMDomain(t *testing.T) {
+	rName := "tf-domain-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+			// Create and Read testing
 			{
-				Config: providerConfig + `
+				Config: providerConfig + fmt.Sprintf(`
 resource "ciphertrust_domain" "testDomain" {
-  name = "domain_test"
+  name = "%s"
   admins = ["admin"]
   allow_user_management = false
   meta_data = {
       "abc": "xyz"
   }
 }
-`,
+`, rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("ciphertrust_domain.testDomain", "id"),
+					resource.TestCheckResourceAttr("ciphertrust_domain.testDomain", "name", rName),
 				),
 			},
+			// Update and Read testing
 			{
-				Config: providerConfig + `
+				Config: providerConfig + fmt.Sprintf(`
 resource "ciphertrust_domain" "testDomain" {
-  name = "domain_test"
+  name = "%s"
   admins = ["admin"]
   allow_user_management = false
   meta_data = {
@@ -36,9 +42,10 @@ resource "ciphertrust_domain" "testDomain" {
 	  "color": "blue"
   }
 }
-`,
+`, rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("ciphertrust_domain.testDomain", "id"),
+					resource.TestCheckResourceAttr("ciphertrust_domain.testDomain", "name", rName),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
