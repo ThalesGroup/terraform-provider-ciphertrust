8b156b45a88935a645833381e6525582f97e9b67
root <manish-n.kumar@external.thalesgroup.com>
2026-08-18 04:59:10 +0000

    Restore plan-time immutable-error test expectation for name rename
    Merging 1.0.1 into this branch silently reverted this test step back
    to its pre-fix form (ConfigPlanChecks/ExpectResourceAction expecting a
    destroy-before-create), even though the schema fix itself
    (ImmutableString() on name) survived the merge intact. Restored the
    PlanOnly + ExpectError(immutable) expectation that actually matches
    current behavior; verified live.

--- internal/provider/resource_cte_process_set_test.go
+++ internal/provider/resource_cte_process_set_test.go
@@ -2,12 +2,12 @@ package provider
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/ThalesGroup/terraform-provider-ciphertrust/internal/provider/common"
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 )
 
 // cteProcessSetConfig renders a ciphertrust_cte_process_set. When secondProcess
@@ -100,15 +100,9 @@ func TestCTEProcessSetResource_nameImmutable(t *testing.T) {
 				),
 			},
 			{
-				Config: cteProcessSetConfig(name+"-renamed", "Original", false),
-				ConfigPlanChecks: resource.ConfigPlanChecks{
-					PreApply: []plancheck.PlanCheck{
-						plancheck.ExpectResourceAction(rn, plancheck.ResourceActionDestroyBeforeCreate),
-					},
-				},
-				Check: checkStep(t, "process_set requires replace: rename",
-					resource.TestCheckResourceAttr(rn, "name", name+"-renamed"),
-				),
+				Config:      cteProcessSetConfig(name+"-renamed", "Original", false),
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(`(?i)immutable`),
 			},
 		},
 	})
