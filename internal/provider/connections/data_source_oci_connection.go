eeca07dc24d4661d0a9dbaaf1b13680edc0a233f
Zubeen <syed-zubeen.qadry@cplcloud.com>
2026-08-21 15:49:09 +0530

    Revert "Fix forcetypeassert: check type assertion when building oci filter query"
    This reverts commit 1fcf299b44e367ed5b3774e93eba6f5c0c71ce8c.

--- internal/provider/connections/data_source_oci_connection.go
+++ internal/provider/connections/data_source_oci_connection.go
@@ -163,15 +163,7 @@ func (d *dataSourceOCIConnection) Read(ctx context.Context, req datasource.ReadR
 	var kvs []string
 	if !state.Filters.IsNull() && !state.Filters.IsUnknown() {
 		for k, v := range state.Filters.Elements() {
-			strVal, ok := v.(types.String)
-			if !ok || strVal.IsNull() || strVal.IsUnknown() {
-				resp.Diagnostics.AddError(
-					"Invalid filters input",
-					fmt.Sprintf("Key %q in filters has an invalid or unconfigured string value", k),
-				)
-				return
-			}
-			kv := fmt.Sprintf("%s=%s&", k, strVal.ValueString())
+			kv := fmt.Sprintf("%s=%s&", k, v.(types.String).ValueString())
 			kvs = append(kvs, kv)
 		}
 	}
